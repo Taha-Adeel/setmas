4da5887cf5c2bd9a2b200cca958df0e0380bccad
prashanthsriram-s <prashanthsriram10@gmail.com>
2023-04-25 15:51:26 +0530

    Added Calendar and Filter Dropdown

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ChartistGraph from "react-chartist";
 import GoogleButton from "./GoogleButton";
+import {Calendar, momentLocalizer} from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 // react-bootstrap components
 import {
   Badge,
@@ -15,12 +18,125 @@ import {
   Form,
   OverlayTrigger,
   Tooltip,
+  Dropdown,
 } from "react-bootstrap";
 
 
 
+const localizer = momentLocalizer(moment)
+// currently 
+const myEventsList = [
+  {
+    'title': 'ALH1: All Day Event very long title',
+    'allDay': true,
+    'start': new Date(2023, 3, 0),
+    'end': new Date(2023, 3, 1),
+    'desc': 'can ya see this'
+  },
+  {
+    'title': 'CLH2: Long Event',
+    'start': new Date(2023, 3, 7),
+    'end': new Date(2023, 3, 10)
+  },
+
+  {
+    'title': 'B102: DTS STARTS',
+    'start': new Date(2016, 2, 13, 0, 0, 0),
+    'end': new Date(2016, 2, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'B102: DTS ENDS',
+    'start': new Date(2016, 10, 6, 0, 0, 0),
+    'end': new Date(2016, 10, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'B102: Some Event',
+    'start': new Date(2023, 3, 9, 0, 0, 0),
+    'end': new Date(2023, 3, 9, 0, 0, 0)
+  },
+  {
+    'title': 'B102: Conference',
+    'start': new Date(2023, 3, 11),
+    'end': new Date(2023, 3, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    'title': 'B102: Meeting',
+    'start': new Date(2023, 3, 12, 10, 30, 0, 0),
+    'end': new Date(2023, 3, 12, 12, 30, 0, 0),
+    desc: 'B102: Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Audi: Lunch',
+    'start': new Date(2023, 3, 12, 12, 0, 0, 0),
+    'end': new Date(2023, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'A520: Meeting',
+    'start': new Date(2023, 3, 12, 14, 0, 0, 0),
+    'end': new Date(2023, 3, 12, 15, 0, 0, 0)
+  },
+  {
+    'title': 'C362: Happy Hour',
+    'start': new Date(2023, 3, 12, 17, 0, 0, 0),
+    'end': new Date(2023, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    'title': 'OAT: Dinner',
+    'start': new Date(2023, 3, 12, 20, 0, 0, 0),
+    'end': new Date(2023, 3, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Audi: Birthday Party',
+    'start': new Date(2023, 3, 13, 7, 0, 0),
+    'end': new Date(2023, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'B612: Birthday Party 2',
+    'start': new Date(2023, 3, 13, 7, 0, 0),
+    'end': new Date(2023, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'B222: Birthday Party 3',
+    'start': new Date(2023, 3, 13, 7, 0, 0),
+    'end': new Date(2023, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'C302: Late Night Event',
+    'start': new Date(2023, 3, 17, 19, 30, 0),
+    'end': new Date(2023, 3, 18, 2, 0, 0)
+  },
+  {
+    'title': 'A112: Multi-day Event',
+    'start': new Date(2023, 3, 20, 19, 30, 0),
+    'end': new Date(2023, 3, 22, 2, 0, 0)
+  }
+];
+
+function CalendarWrapper({eventList, venue}){
+  if(venue !=='ALL')
+    return (<Calendar
+    localizer={localizer}
+    events={eventList.filter(event => event.title.startsWith(venue))}
+    startAccessor="start"
+    endAccessor="end"
+    style={{ height: 500 }}
+    />);
+  else return (<Calendar
+    localizer={localizer}
+    events={eventList}
+    startAccessor="start"
+    endAccessor="end"
+    style={{ height: 500 }}
+  />);;
+}
 
 function Dashboard() {
+  const [roomstate, setRoomState] = useState('ALL');
   return (
     <>
       <Container fluid>
@@ -147,7 +263,44 @@ function Dashboard() {
             <GoogleButton />
           </Card.Body>
         </Card>
+        <Card>
+          <Card.Header>
+            <Card.Title as="h4">Calendar</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <label>Select Room </label><Dropdown>
+              <Dropdown.Toggle
+                as={Nav.Link}
+                data-toggle="dropdown"
+                id="dropdown-67443507"
+                variant="default"
+                name="room"
+                className="m-0"
+              >
 
+                <span className="notification">{roomstate}</span>
+                <span className="d-lg-none ml-1">Notification</span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={(e) => { setRoomState("ALL");  }}>All Rooms</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("ALH1"); }}>ALH1</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("A112"); }}>A112</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("B102"); }}>B102</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("CLH2"); }}>CLH2</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("Audi"); }}>Audi</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => { setRoomState("B115"); }}>B115</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <CalendarWrapper eventList={myEventsList} venue={roomstate} />
+            {/* <Calendar
+              localizer={localizer}
+              events={myEventsList}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500 }}
+            /> */}
+          </Card.Body>
+        </Card>
         {/* <Row>
           <Col md="8">
             <Card>
