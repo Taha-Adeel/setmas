0804f051a51b16ce1e6754be1db591dda86f8825
Taha_Adeel <cs20btech11052@iith.ac.in>
2023-05-01 04:06:52 +0530

    Made mailer into a class

--- backend/src/util/mailer.py
+++ backend/src/util/mailer.py
@@ -1,59 +1,70 @@
-from flask import Flask
+import os
 from flask_mail import Mail, Message
+from app import app
 
-app = Flask(__name__)
+class Mailer:
+    _instance = None
 
-# Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'setmasiith@gmail.com'
-app.config['MAIL_PASSWORD'] = 'nkryqhcbrabdgwkn'
+    mail_server = 'smtp.gmail.com'
+    mail_port = 587
+    mail_use_tls = True
+    mail_username = 'setmasiith@gmail.com'
+    mail_password = 'nkryqhcbrabdgwkn'
 
-mail = Mail(app)
+    mail = Mail(app)
 
-def request_submission_notif(email):
-    msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
-    msg.body = 'Dear User,\nYour booking request has been submitted.'
-    mail.send(msg)
-    return 'Email sent'
+    def _init_config():
+        app.config['MAIL_SERVER'] = Mailer.mail_server
+        app.config['MAIL_PORT'] = Mailer.mail_port
+        app.config['MAIL_USE_TLS'] = Mailer.mail_use_tls
+        app.config['MAIL_USERNAME'] = Mailer.mail_username
+        app.config['MAIL_PASSWORD'] = Mailer.mail_password
 
-def request_accepted_notif(email):
-    msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
-    msg.body = 'Dear User,\nYour booking request has been accepted.'
-    mail.send(msg)
-    return 'Email sent'
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            Mailer._init_config()
+            cls._instance = super(Mailer, cls).__new__(cls, *args, **kwargs)
 
-def request_rejected_notif(email):
-    msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
-    msg.body = 'Dear User,\nYour booking request has been rejected.'
-    mail.send(msg)
-    return 'Email sent'
+        return cls._instance
 
-def new_admin_status(email):
-    msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[email])
-    msg.body = 'Dear User,\nYou are now an admin.'
-    mail.send(msg)
-    return 'Email sent'
+    def request_submission_notif(email):
+        msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
+        msg.body = 'Dear User,\nYour booking request has been submitted.'
+        Mailer.mail.send(msg)
+        return 'Email sent'
 
-def delete_admin_status(email):
-    msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[email])
-    msg.body = "Dear User,\nYou've been removed as an admin."
-    mail.send(msg)
-    return 'Email sent'
+    def request_accepted_notif(email):
+        msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
+        msg.body = 'Dear User,\nYour booking request has been accepted.'
+        Mailer.mail.send(msg)
+        return 'Email sent'
 
-def super_admin_status(new_superAdmin_email, old_superAdmin_email):
-    msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[new_superAdmin_email])
-    msg.body = "Dear User,\nYou've been promoted to Super Admin."
-    mail.send(msg)
+    def request_rejected_notif(email):
+        msg = Message(subject='Booking Request Status', sender='setmasiith@gmail.com', recipients=[email])
+        msg.body = 'Dear User,\nYour booking request has been rejected.'
+        Mailer.mail.send(msg)
+        return 'Email sent'
 
-    msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[old_superAdmin_email])
-    msg.body = "Dear User,\nYou've been demoted to Admin."
-    mail.send(msg)
+    def new_admin_status(email):
+        msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[email])
+        msg.body = 'Dear User,\nYou are now an admin.'
+        Mailer.mail.send(msg)
+        return 'Email sent'
+
+    def delete_admin_status(email):
+        msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[email])
+        msg.body = "Dear User,\nYou've been removed as an admin."
+        Mailer.mail.send(msg)
+        return 'Email sent'
+
+    def super_admin_status(new_superAdmin_email, old_superAdmin_email):
+        msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[new_superAdmin_email])
+        msg.body = "Dear User,\nYou've been promoted to Super Admin."
+        Mailer.mail.send(msg)
+
+        msg = Message(subject='Change in Status', sender='setmasiith@gmail.com', recipients=[old_superAdmin_email])
+        msg.body = "Dear User,\nYou've been demoted to Admin."
+        Mailer.mail.send(msg)
 
 
-    return 'Email sent'
-
-
-if __name__ == '__main__':
-   app.run(debug = True)
+        return 'Email sent'
